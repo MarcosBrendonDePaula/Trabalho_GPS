c9c85d5deb9ab7fb95e0048b6a330613445cc1b1
marcos brendon de paula azevedo <marcos.brendon@aluno.ufms.br>
2019-10-01 15:20:45 -0300

    updatecheck

--- Index/autentication.js
+++ Index/autentication.js
@@ -1,15 +1,14 @@
+const Email=document.getElementById("TxtEmail")
+const BtnLog=document.getElementById("btnlog")
+const TxtSenha=document.getElementById("txtsenha")
+
+const expEmail = new RegExp("^[a-z0-9]+@[a-z]+.com$")
 const check=()=>{
-    const Email=document.getElementById("TxtEmail")
-    const BtnLog=document.getElementById("btnlog")
-    Email.value="";
-    BtnLog.disabled  = true;
-    Email.addEventListener('keydown',()=>{
-        let conteudo = Email.value.toString();
-        if(parseInt(conteudo.indexOf('@'))===-1){
-            console.debug("Falta @"+conteudo.indexOf('@'));
-            BtnLog.disabled  = true;
-        }else
-            BtnLog.disabled  = false;
-    })
+    var pas=TxtSenha.value
+    var atual = expEmail.test(Email.value)*pas.length
+    console.log(Email.value+" "+expEmail.test(Email.value))
+    if(atual>=1)
+        BtnLog.disabled = false 
 }
-check();
+BtnLog.addEventListener("mouseenter",check)
+BtnLog.disabled = true
